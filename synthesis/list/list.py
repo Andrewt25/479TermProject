cf6590f4c70a42610d31290106392327d2acc6d3
sypan <stephenyuehpan@gmail.com>
2023-03-31 22:16:22 -0700

    change ListTo init and list test file structure

--- synthesis/list/list.py
+++ synthesis/list/list.py
@@ -10,13 +10,12 @@ from .methods.for_loop import *
 
 class ListTo():
 
-  def __init__(self, ast_obj: ast.AST, target_type: DataType) -> None:
+  def __init__(self, ast_obj: ast.AST) -> None:
     self.type = DataType.List
     self.ast_obj = ast_obj
-    self.target_type = target_type
 
-  def modify_ast(self) -> list:
-    if self.target_type == self.type:
+  def modify_ast(self, target_type: DataType) -> list:
+    if target_type == self.type:
       return []
 
     # Find all variables that is type list
@@ -29,19 +28,19 @@ class ListTo():
       tree = copy.deepcopy(self.ast_obj)
 
       # Modify all variables that is a type list to target data type
-      var_transformer = VariableTransformer(var, DataType.List, self.target_type)
+      var_transformer = VariableTransformer(var, self.type, target_type)
       tree = var_transformer.visit(tree)
 
-      append_visitor = AppendVisitor(var, self.target_type)
+      append_visitor = AppendVisitor(var, target_type)
       tree = append_visitor.visit(tree)
       
-      count_visitor = CountVisitor(var, self.target_type)
+      count_visitor = CountVisitor(var, target_type)
       tree = count_visitor.visit(tree)
 
-      remove_visitor = RemoveVisitor(var, self.target_type)
+      remove_visitor = RemoveVisitor(var, target_type)
       tree = remove_visitor.visit(tree)
 
-      for_visitor = ForVisitor(var, self.target_type)
+      for_visitor = ForVisitor(var, target_type)
       tree = for_visitor.visit(tree)
 
       trees.append(tree)
--- test/list/test_list_to.py
+++ test/list/test_list_to.py
@@ -7,26 +7,23 @@ from test.util import *
 class TestListTo(unittest.TestCase):
 
   def setUp(self):
-    filename = 'test/list/example.py'
+    filename = 'test/list/input_list.py'
     with open(filename) as f:
       self.input_tree = ast.parse(f.read(), filename=filename)
+    self.list_to = ListTo(self.input_tree)
 
   def test_dict(self):
-    filenames = ['test/list/output_dict_0.py', 'test/list/output_dict_1.py']
+    filenames = ['test/list/output_dict/output_dict_0.py', 'test/list/output_dict/output_dict_1.py']
     expected_output_tree = get_expected_output_trees(filenames)
 
-    list_to = ListTo(self.input_tree, DataType.Dict)
-    trees = list_to.modify_ast()
-    for output_tree in trees:
+    for output_tree in self.list_to.modify_ast(DataType.Dict):
       self.assertTrue(ast.dump(output_tree) in expected_output_tree)
 
   def test_set(self):
-    filenames = ['test/list/output_set_0.py', 'test/list/output_set_1.py']
+    filenames = ['test/list/output_set/output_set_0.py', 'test/list/output_set/output_set_1.py']
     expected_output_tree = get_expected_output_trees(filenames)
 
-    list_to = ListTo(self.input_tree, DataType.Set)
-    trees = list_to.modify_ast()
-    for output_tree in trees:
+    for output_tree in self.list_to.modify_ast(DataType.Set):
       self.assertTrue(ast.dump(output_tree) in expected_output_tree)
 
 
