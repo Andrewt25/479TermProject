5a052fb0ce2b59506e1eab6d4250d899dd0543be
Andrewt25 <25161127+Andrewt25@users.noreply.github.com>
2023-04-08 12:32:09 -0700

    check for duplicate modifications and add dictTo

--- synthesis/index.py
+++ synthesis/index.py
@@ -3,12 +3,14 @@ import os
 import ast
 import timeit
 import matplotlib.pyplot as plt
+import hashlib
 
 file_path = os.path.abspath(__file__)
 sys.path.append(os.path.dirname(os.path.dirname((file_path))))
 
 from datatypes import *
 from list.list import *
+from dict.dict import *
 from testTransformer import *
 
 
@@ -16,9 +18,11 @@ def testDriver(programPath:str, unitPath: str):
     programFileName = getFileName(programPath)
 
     # Build tests base -> 100 increment 10 at a time 11 total
-    performanceTests = [] 
+    performanceTests = []
+    encounteredModifications = set()
     
-
+    # Generate n modified versions of suplied unit test incrementing complexity by 10.
+    # i.e. dupicating operation 10 times up to n*10 times
     for i in range(101):
         transformer =  testTransformer(programFileName, i*10 )
         unitTree = getAST(unitPath)
@@ -33,44 +37,65 @@ def testDriver(programPath:str, unitPath: str):
     initial =()
     index = 1
 
+    '''
+    First iteration program runs the generated test against the unmodfied program ast and records results. It then finds the possible changes that can be made
+    to the ast and passes asts with those changes into the next iteration.
+    Second iteration onwards the test are run against all the asts supplied by the previous iteration using the ast with the smallest growth rate and find asts
+    for further exploration
+    '''
     while True:
         topIterPerformer = ()
         for modAst in astsToTest:
-            # warmup
+            # warmup to avoid slow intial runs
             combineAndExecute(copy.deepcopy(modAst), performanceTests[0])
             times = []
+            # execute all generated tests against modified program
             for test in performanceTests:
                 times = times + combineAndExecute(copy.deepcopy(modAst), test)
-                #record time
-                #output
+            # check for times if none returned execution failed
             if len(times) > 0:
+                # set initial on first execution for unmodified program
                 if(len(initial) == 0):
                     rate = avgGrowthRate(times)
                     initial = (modAst, times, rate)
+                # comparison to find best iteration performer
                 topIterPerformer = comparePerformance(topIterPerformer, times, modAst)
-
-        topPerformer = comparePerformance(topPerformer, topIterPerformer[1], topIterPerformer[0])
+        # comparison to find best overall performer
+        if(len(topIterPerformer) == 3):
+            topPerformer = comparePerformance(topPerformer, topIterPerformer[1], topIterPerformer[0])
 
         # reset list after tests
         astsToTest = []
+        # Find modifications if any for each supported type
         for type in DataType:
+            # checks that there were valid results this iteration
             if(len(topIterPerformer) == 3):
+                # Get possible changes for supported types
                 treeProgram = topIterPerformer[0]
-                modAsts = ListTo(copy.deepcopy(treeProgram)).modify_ast(type)
-                astsToTest = astsToTest + modAsts
-                if(len(modAsts) > 0):
-                    with open("ast"+type.name+".txt", "w") as f:
-                        f.write(ast.unparse(modAsts[0]))
-                        f.close()
+                modListAsts = ListTo(copy.deepcopy(treeProgram)).modify_ast(type)
+                astsToTest = astsToTest + modListAsts
+                modDictAsts = DictTo(copy.deepcopy(treeProgram)).modify_ast(type)
+                astsToTest = astsToTest + modDictAsts
+        
+        # check for duplicate asts
+        for modAst in astsToTest:
+            ast_str = ast.dump(modAst)
+            hash_obj = hashlib.sha256(ast_str.encode())
+            hash = hash_obj.hexdigest()
+            if(hash in encounteredModifications):
+                astsToTest.remove(modAst)
+            else:
+                encounteredModifications.add(hash)
+
 
         index = index + 1
         # program completes at a depth of 50 or all branches explored
-        if astsToTest == [] or index == 50:
+        if astsToTest == [] or index == 10:
             break
     createOutput(initial[1], topPerformer[1])
     writeSuggestion(topPerformer[0])
         
-
+# combines two asts into single ast and execute the results
 def combineAndExecute(modAst, test):
     # combine imports and body of modified program and test
     for n in test.importNodes:
